972f0836aabb89987390fbf1ee0e2efbd20ac8d3
ryan anzer <ryan.anzer@epitech.eu>
2018-07-16 23:34:25 +0200

    fast save

--- src/packet/buffer/frontierReq.js
+++ src/packet/buffer/frontierReq.js
@@ -2,7 +2,10 @@
 
 module.exports = (header, d) => {
 	const buf = Buffer.alloc(8);
-	buf.writeInt32BE(d.age, 0);
-	buf.writeInt32BE(d.account, 0);
-	return Buffer.concat([Buffer.from(d.start, 'hex'), buf]);
+	buf.writeInt32BE(d.age || 0, 0);
+	buf.writeInt32BE(d.count || 0, 0);
+	return Buffer.concat([
+		d.start instanceof Buffer ? d.start : Buffer.from(d.start, 'hex'),
+		buf
+	]);
 };
