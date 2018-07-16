792a29b5c7e50139c51b50e4775bf6c25a6ad2f2
ryan anzer <ryan.anzer@epitech.eu>
2018-07-16 23:44:38 +0200

    fast save

--- src/packet/buffer/frontierReq.js
+++ src/packet/buffer/frontierReq.js
@@ -5,6 +5,7 @@ module.exports = (header, d) => {
 	buf.writeUIntLE(d.age || 0, 0, 4);
 	buf.writeUIntLE(d.count || 0, 4, 4);
 	return Buffer.concat([
+		header,
 		d.start instanceof Buffer ? d.start : Buffer.from(d.start, 'hex'),
 		buf
 	]);
